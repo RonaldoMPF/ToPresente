2f0a8f7c44205d2c5e3ff64939369c3740b28f19
Benardi <benardinunes@gmail.com>
2018-07-01 11:52:57 -0300

    Conform files to pep8.

--- to_presente/tests/unit/test_shell.py
+++ to_presente/tests/unit/test_shell.py
@@ -15,16 +15,17 @@
 
 import unittest
 
+
 class TestShell(unittest.TestCase):
- 
+
     def setUp(self):
         pass
- 
+
     def test_numbers_3_4(self):
-        self.assertEqual( 3 *4 , 12)
- 
+        self.assertEqual(3 * 4, 12)
+
     def test_strings_a_3(self):
-        self.assertEqual( 3*'a', 'aaa')
- 
+        self.assertEqual(3 * 'a', 'aaa')
+
 if __name__ == '__main__':
     unittest.main()
